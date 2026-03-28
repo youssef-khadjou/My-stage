0518ebaee9f13979b814ca0b0a0c4481155d1118
El Ouariachi Noureddine <noureddine.el-ouariachi@etu.univ-orleans.fr>
2026-03-28 17:22:41 +0100

    accueil etudiant

--- my-stage/frontend/src/app/app.routes.ts
+++ my-stage/frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Login } from './auth/login/login';
 import { Register } from './auth/register/register';
+import { Dashboard as EtudiantDashboard } from './etudiant/dashboard/dashboard';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'login/:role', component: Login },
-  { path: 'register', component: Register }
+  { path: 'register', component: Register },
+  { path: 'etudiant', component: EtudiantDashboard }
 ];
