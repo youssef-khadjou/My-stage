a100158831a78bad17195580d9accbe92ae35c09
El Ouariachi Noureddine <noureddine.el-ouariachi@etu.univ-orleans.fr>
2026-03-28 17:04:39 +0100

    Page de connexion

--- my-stage/frontend/src/app/app.routes.ts
+++ my-stage/frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Login } from './auth/login/login';
 
 export const routes: Routes = [
-  { path: '', component: Home }
+  { path: '', component: Home },
+  { path: 'login/:role', component: Login }
 ];
--- my-stage/frontend/src/app/auth/login/login.ts
+++ my-stage/frontend/src/app/auth/login/login.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
-export class Login {}
+export class Login implements OnInit {
+
+  role: string = '';
+  email: string = '';
+  motDePasse: string = '';
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit() {
+    this.role = this.route.snapshot.paramMap.get('role') || '';
+  }
+
+  seConnecter() {
+    console.log('Connexion avec', this.email, 'role:', this.role);
+  }
+
+}
