70fdbfd1073a00b078ab1bc17fb92c4f1e7e485e
El Ouariachi Noureddine <noureddine.el-ouariachi@etu.univ-orleans.fr>
2026-03-28 17:18:15 +0100

    Page d'inscription

--- my-stage/frontend/src/app/app.routes.ts
+++ my-stage/frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'login/:role', component: Login }
+  { path: 'login/:role', component: Login },
+  { path: 'register', component: Register }
 ];
--- my-stage/frontend/src/app/auth/register/register.ts
+++ my-stage/frontend/src/app/auth/register/register.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './register.html',
-  styleUrl: './register.css',
+  styleUrl: './register.css'
 })
-export class Register {}
+export class Register {
+
+  nom: string = '';
+  prenom: string = '';
+  sexe: string = '';
+  fonction: string = '';
+  departement: string = '';
+  email: string = '';
+  motDePasse: string = '';
+  confirmation: string = '';
+
+  constructor(private router: Router) {}
+
+  creerCompte() {
+    console.log('Création compte pour', this.email, 'fonction:', this.fonction);
+  }
+
+}
