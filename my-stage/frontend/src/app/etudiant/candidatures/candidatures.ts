c1ba7bf8ca40c951c25abfe3aa28e94454b8a1ee
El Ouariachi Noureddine <noureddine.el-ouariachi@etu.univ-orleans.fr>
2026-03-29 04:50:47 +0200

    page confirmer mon stage etudiant

--- my-stage/frontend/src/app/etudiant/candidatures/candidatures.ts
+++ my-stage/frontend/src/app/etudiant/candidatures/candidatures.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-candidatures',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './candidatures.html',
-  styleUrl: './candidatures.css',
+  styleUrl: './candidatures.css'
 })
-export class Candidatures {}
+export class Candidatures {
+
+  constructor(private router: Router) {}
+
+  stage = {
+    entreprise: '',
+    poste: '',
+    debut: '',
+    fin: '',
+    gratification: '',
+    tuteur: '',
+    lieu: ''
+  };
+
+  confirmer() {
+    console.log('Stage confirmé');
+  }
+
+  refuser() {
+    console.log('Stage refusé');
+  }
+
+}
